d326fb22f22999fe0ba735b3f39b14cf949e1119
Philip Tesch <philiptesch99@gmail.com>
2025-03-19 21:27:27 +0100

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule,RouterModule],
+  imports: [MatButtonModule, RouterModule, HeaderComponent,MatInputModule,MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
