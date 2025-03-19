cff1584db6ec1605b22c148349b57894bb2c196d
Erich Getinger <erich.getinger@outlook.de>
2025-03-19 08:42:49 +0100

    neu initialisierung des projektes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-b6814","appId":"1:861945353229:web:0f2053b392728ba732b144","storageBucket":"dabubble-b6814.firebasestorage.app","apiKey":"AIzaSyCp6yNSTyFQGZb7u2-X5oRWscDapMT9J6M","authDomain":"dabubble-b6814.firebaseapp.com","messagingSenderId":"861945353229"})), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatButtonModule,RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
