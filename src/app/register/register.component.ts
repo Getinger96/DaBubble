f8552deb749baed3f933312eb517d6dc4f0f3119
Erich Getinger <erich.getinger@outlook.de>
2025-03-19 09:58:48 +0100

    register bearbeitet inputs eingefügt

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
