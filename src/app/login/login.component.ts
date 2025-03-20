8f6e23d86db7cb9b679203fc7ec7765bd1a055b8
Philip Tesch <philiptesch99@gmail.com>
2025-03-20 22:21:53 +0100

    login-input

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,13 +4,28 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, RouterModule, HeaderComponent,MatInputModule,MatIconModule],
+  imports: [MatButtonModule, RouterModule, HeaderComponent,MatInputModule,MatIconModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  mailFieldIsActive: boolean = false;
+  emailIconIsActive: boolean = false;
 
+
+
+  onInputEmailClick() {
+    this.mailFieldIsActive = true
+    this.emailIconIsActive =  true
+  }
+
+
+  onInputEmailBlur() {
+    this.mailFieldIsActive = false
+    this.emailIconIsActive =  false
+  }
 }
