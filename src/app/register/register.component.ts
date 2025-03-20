bbc3b4fc5612a8077ea91e35c329e89a71e4686d
Erich Getinger <erich.getinger@outlook.de>
2025-03-20 22:50:42 +0100

    changed height of register and added header

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,13 +4,15 @@ import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule,MatButtonModule],
+  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule,MatButtonModule,RouterModule,HeaderComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
