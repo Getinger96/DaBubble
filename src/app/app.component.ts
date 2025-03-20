08c5b56acdc7ee9f5a0a92c3ba5f4ffb6dd63e73
Erich Getinger <erich.getinger@outlook.de>
2025-03-20 22:01:42 +0100

    changed border of input fields in register and added a button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-import { HeaderComponent } from "./header/header.component"
+import { HeaderComponent } from "./header/header.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,17 +1,33 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule],
+  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule,MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+borderVisibleName:boolean=false;
+borderVisibleMail:boolean=false;
+borderVisiblepasswort:boolean=false;
+divfocusName(){
+  this.borderVisibleName=true
+}
 
+divfocusmail(){
+  this.borderVisibleMail=true
+}
+
+
+divfocusPasswort(){
+  this.borderVisiblepasswort=true
+}
 }
