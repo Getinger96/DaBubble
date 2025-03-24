889b94f7194e5c55ef493ea4e55d37521ae5af98
Erich Getinger <erich.getinger@outlook.de>
2025-03-24 18:56:40 +0100

    form validation for register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../interfaces/user.interface';
 import { RegisterService } from '../firebase-services/register.service';
 
@@ -15,37 +15,46 @@ import { RegisterService } from '../firebase-services/register.service';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatInputModule,CommonModule,MatCardModule,MatIconModule,MatButtonModule,RouterModule,HeaderComponent,FormsModule],
+  imports: [MatInputModule, CommonModule, MatCardModule, MatIconModule, MatButtonModule, RouterModule, HeaderComponent, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-borderVisibleName:boolean=false;
-borderVisibleMail:boolean=false;
-borderVisiblepasswort:boolean=false;
-user:User = new User()
+  borderVisibleName: boolean = false;
+  borderVisibleMail: boolean = false;
+  borderVisiblepasswort: boolean = false;
+  user: User = new User();
+  isChecked: boolean = false
 
 
-constructor(private registerservice: RegisterService){
+  constructor(private registerservice: RegisterService) {
 
-}
+  }
 
-divfocusName(field: string, isFocused: boolean){
-  this.borderVisibleName=isFocused
-}
+  divfocusName(field: string, isFocused: boolean) {
+    this.borderVisibleName = isFocused
+  }
 
-divfocusmail(field: string, isFocused: boolean){
-  this.borderVisibleMail=isFocused
-}
+  divfocusmail(field: string, isFocused: boolean) {
+    this.borderVisibleMail = isFocused
+  }
 
 
-divfocusPasswort(field: string, isFocused: boolean){
-  this.borderVisiblepasswort=isFocused
-}
-addUser(){
-  
-  this.registerservice.addNewUser(this.registerservice.setUserObject(this.user))
-}
+  divfocusPasswort(field: string, isFocused: boolean) {
+    this.borderVisiblepasswort = isFocused
+  }
+  addUser(ngForm: NgForm) {
+
+    this.registerservice.addNewUser(this.registerservice.setUserObject(this.user))
+
+    ngForm.resetForm()
+
+
+  }
+
+
+
+
 
 
 
