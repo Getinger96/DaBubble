b357055122b169045c5a6d96ed6d094fd0ad270d
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-03-28 18:19:08 +0100

    added search-bar, active-user and webspace-menu

--- src/app/main-components/main-components.component.ts
+++ src/app/main-components/main-components.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../main-components/search-bar/search-bar.component';
-import { ActiveUserComponent } from '../main-components/active-user/active-user.component';
+import { ActiveUserComponent } from '../shared-components/active-user/active-user.component';
 import { WebspaceMenuComponent } from '../main-components/webspace-menu/webspace-menu.component';
 import { MainChatComponent } from '../main-components/main-chat/main-chat.component';
 import { ThreadComponent } from '../main-components/thread/thread.component';
+import { HeaderComponent } from '../shared-components/header/header.component';
 
 @Component({
   selector: 'app-main-components',
   standalone: true,
-  imports: [SearchBarComponent, ActiveUserComponent, WebspaceMenuComponent, MainChatComponent, ThreadComponent],
+  imports: [SearchBarComponent, ActiveUserComponent, WebspaceMenuComponent, MainChatComponent, ThreadComponent, HeaderComponent],
   templateUrl: './main-components.component.html',
   styleUrl: './main-components.component.scss'
 })
