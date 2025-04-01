3c79abf4b1330525196da6fe245c028f31c31fe8
Henrik Petersen <hpet@deop0509.autoconnect.de>
2025-04-01 07:45:27 +0200

    add arrow back in privacy

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared-components/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
