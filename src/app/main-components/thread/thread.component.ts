f2cf7bbf72e41a8ddcf455b10a679c756bdcfdb8
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-04-03 16:26:58 +0200

    created thread window

--- src/app/main-components/thread/thread.component.ts
+++ src/app/main-components/thread/thread.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageComponent } from '../../shared-components/message/message.component';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
