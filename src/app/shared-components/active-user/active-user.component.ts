30bbfc579bb11b9065e08c41b7d39069f8bf4489
Philip Tesch <philiptesch99@gmail.com>
2025-04-09 20:51:36 +0200

    save acutal user

--- src/app/shared-components/active-user/active-user.component.ts
+++ src/app/shared-components/active-user/active-user.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../interfaces/user.interface';
 import { RegisterService } from '../../firebase-services/register.service';
 import { CommonModule } from '@angular/common';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-active-user',
   standalone: true,
@@ -10,16 +10,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './active-user.component.html',
   styleUrl: './active-user.component.scss'
 })
-export class ActiveUserComponent {
+export class ActiveUserComponent implements OnInit {
   actualUser:User[]= []
  overlayvisible:boolean=false;
+ private actualUserSubscription!: Subscription;
 
   constructor(private registerservice: RegisterService){
    
-    this.actualUser=this.registerservice.actualUser
 
   }
 openoverlay(){
   this.overlayvisible=true
+}
+
+ngOnInit(): void {
+  this.actualUserSubscription = this.registerservice.acutalUser$.subscribe(actualUser => {
+    if (actualUser.length > 0) {
+      this.actualUser = actualUser
+      console.log('aktueller User:', this.actualUser);
+    }
+  });
 }
 }
