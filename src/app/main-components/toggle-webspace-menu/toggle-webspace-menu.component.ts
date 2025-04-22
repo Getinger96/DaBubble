5a2efea0a661d34be05f266414799dc2de4a9665
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-04-22 12:19:43 +0200

    implemented wirkspace toggle hover effect

--- src/app/main-components/toggle-webspace-menu/toggle-webspace-menu.component.ts
+++ src/app/main-components/toggle-webspace-menu/toggle-webspace-menu.component.ts
@@ -1,21 +1,26 @@
 import { Component, Output } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle } from '@angular/common';
 import { MainComponentsComponent } from '../main-components.component';
 
 @Component({
   selector: 'app-toggle-webspace-menu',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgStyle],
   templateUrl: './toggle-webspace-menu.component.html',
   styleUrl: './toggle-webspace-menu.component.scss'
 })
+
 export class ToggleWebspaceMenuComponent {
-
-
   mainComponents = MainComponentsComponent;
+  isWorkspaceOpen:boolean = true;
 
  toggleWorkspace(){
     this.mainComponents.toggleWorkspace();
+    if(this.isWorkspaceOpen) {
+      this.isWorkspaceOpen = false;
+    } else if(!this.isWorkspaceOpen) {
+      this.isWorkspaceOpen = true;
+    }
   }
 
 }
