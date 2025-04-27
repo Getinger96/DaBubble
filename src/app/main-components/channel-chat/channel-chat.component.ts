36ccfa51552771fef22bc1bae9d4e8f9ee91f200
Philip Tesch <philiptesch99@gmail.com>
2025-04-27 21:04:07 +0200

    chanel-user-list

--- src/app/main-components/channel-chat/channel-chat.component.ts
+++ src/app/main-components/channel-chat/channel-chat.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, Input } from '@angular/core';
 import { ChannelService } from '../../firebase-services/channel.service';
 import {MatDialog,MatDialogActions,MatDialogClose,MatDialogContent,MatDialogTitle,} from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { ShowUserComponent } from './show-user/show-user.component';
 import {MatIconModule} from '@angular/material/icon';
 import { HostListener } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
+import { NgIf, CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule ],
+  imports: [MatButtonModule,MatIconModule, CommonModule ],
   templateUrl: './channel-chat.component.html',
   styleUrl: './channel-chat.component.scss'
 })
 export class ChannelChatComponent implements OnInit {
   currentChannelName: string = '';
+  @Input() allUsersChannel: User[] = [];
   readonly dialog = inject(MatDialog);
 
 
@@ -28,6 +32,11 @@ export class ChannelChatComponent implements OnInit {
   }
 
   openDialog() {
-    this.dialog.open(ShowUserComponent);
+    this.dialog.open(ShowUserComponent, {
+      data: { allUsersChannel: this.allUsersChannel },
+      panelClass: 'another-dialog-position'
+    });
   }
+
+
 }
--- src/app/main-components/channel-chat/show-user/show-user.component.ts
+++ src/app/main-components/channel-chat/show-user/show-user.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input , Inject } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { User } from '../../../interfaces/user.interface';
+import { NgIf, CommonModule } from '@angular/common';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-show-user',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './show-user.component.html',
   styleUrl: './show-user.component.scss'
 })
 export class ShowUserComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { allUsersChannel: User[] }) {}
 
 }
