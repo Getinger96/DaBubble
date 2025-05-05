5e60f1bfcd2e9e693c2f247eb46a56eecf86a344
Erich Getinger <erich.getinger@outlook.de>
2025-05-05 13:18:41 +0200

    added function to get the (du) span for the logged in user

--- src/app/main-components/workspace-menu/direct-message-user/direct-message-user.component.ts
+++ src/app/main-components/workspace-menu/direct-message-user/direct-message-user.component.ts
@@ -4,6 +4,7 @@ import { User } from '../../../interfaces/user.interface';
 import { RegisterService } from '../../../firebase-services/register.service';
 import { LoadingService } from '../../../services/loading.service';
 import { Subscription } from 'rxjs';
+import { MainComponentService } from '../../../firebase-services/main-component.service';
 
 @Component({
   selector: 'app-direct-message-user',
@@ -13,16 +14,41 @@ import { Subscription } from 'rxjs';
   styleUrl: './direct-message-user.component.scss'
 })
 export class DirectMessageUserComponent implements OnInit {
-
-
+actualUser:User[]= [];
   @Input() ownAccount!:boolean;
   @Input() userArray!: User[];
   private usersSubscription!: Subscription;
-  constructor(private registerservice: RegisterService, private loadingService : LoadingService){
+  private actualUserSubscription!: Subscription;
+  constructor(private registerservice: RegisterService, private loadingService : LoadingService,private mainservice:MainComponentService){
 
   }
 
   ngOnInit(): void {
+ this.loadActualUser()
  
+
   }
+
+  loadActualUser(){
+    this.actualUserSubscription = this.mainservice.acutalUser$.subscribe(actualUser => {
+      if (actualUser.length > 0) {
+        this.actualUser = actualUser
+     
+        console.log('actualUser[0]', actualUser[0]);
+        
+      }
+    });
+    this.sortUsers()
+  }
+
+  sortUsers(){
+    this.userArray.sort((a,b)=>{
+      if(a.id===this.actualUser[0].id)return-1;
+      if (b.id===this.actualUser[0].id)return 1;
+      return 0;
+        
+      
+    })
+  }
+
 }
