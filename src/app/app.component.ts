d90391a11ebc49351ca6eb31d989ae2a37c6b09c
Philip Tesch <philiptesch99@gmail.com>
2025-05-12 23:19:35 +0200

    app-channel chat design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ChannelChatComponent } from "./main-components/channel-chat/channel-chat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ChannelChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
